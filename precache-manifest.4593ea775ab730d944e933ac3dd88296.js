4ccaf394b9b6f6ec57338d7d728c5bfe87226dde
Mayur290 <mayurbansal98@gmail.com>
2020-12-06 06:18:19 +0530

    Updates

---
+++ precache-manifest.4593ea775ab730d944e933ac3dd88296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f63910f518a0e980d991d4d898cc60",
+    "url": "/BugTracker-Blockchain/index.html"
+  },
+  {
+    "revision": "c4aad6e4c4aa79de6432",
+    "url": "/BugTracker-Blockchain/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "9db63b4378e59d45443b",
+    "url": "/BugTracker-Blockchain/static/css/main.c6627bf0.chunk.css"
+  },
+  {
+    "revision": "c4aad6e4c4aa79de6432",
+    "url": "/BugTracker-Blockchain/static/js/2.d86080e3.chunk.js"
+  },
+  {
+    "revision": "f0f12cb8922166a481ab7756fc3c6515",
+    "url": "/BugTracker-Blockchain/static/js/2.d86080e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db63b4378e59d45443b",
+    "url": "/BugTracker-Blockchain/static/js/main.6d5dfb94.chunk.js"
+  },
+  {
+    "revision": "5f7804f77a2165c0f866",
+    "url": "/BugTracker-Blockchain/static/js/runtime-main.be302d13.js"
+  }
+]);
